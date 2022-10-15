bc309ef74a26a20ee135514bc5361b7e367e4bcb
Saisha <78188508+SaishaAmrutham@users.noreply.github.com>
2022-10-15 13:19:55 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload() {
+
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent('canvas')
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
